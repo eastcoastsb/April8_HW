a5420de1643809af608f92dbe4bcd404a745c03f
Ryan McGovern <ryanmcgovern@Ryans-MacBook-Air.local>
2014-04-09 09:04:18 -0400

    Initial commit

---
+++ site.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+
+	$(".thumbnail1").click(function(event) {
+		event.preventDefault();
+		if($(".albumNav").parent().hasClass("hiddenAlbum")) {
+			$(".albumNav").parent().toggleClass("hiddenAlbum");
+			
+		} else {
+			$(".albumNav").parent().toggleClass("hiddenAlbum");
+		}
+
+	});
+
+	$(".thumbnail2").click(function(event) {
+		event.preventDefault();
+		if($(".albumNav").parent().hasClass("hiddenAlbum")) {
+			$(".albumNav").parent().toggleClass("hiddenAlbum");
+			
+		} else {
+			$(".albumNav").parent().toggleClass("hiddenAlbum");
+		}
+	});
+
+	$(".thumbnail3").click(function(event) {
+		event.preventDefault();
+		if($(".albumNav").parent().hasClass("hiddenAlbum")) {
+			$(".albumNav").parent().toggleClass("hiddenAlbum");
+			
+		} else {
+			$(".albumNav").parent().toggleClass("hiddenAlbum");
+		}
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
